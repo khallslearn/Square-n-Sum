52379fbcb1b3c0cfbf7df3fb4a5cabfa6da03c4b
KH <75588413+khallslearn@users.noreply.github.com>
2022-08-06 15:15:23 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/*Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.*/
+
+function squareSum(numbers){
+    var result = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      result += (numbers[i] * numbers[i]);
+    }
+    return result;
+  }
